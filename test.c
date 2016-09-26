6203cb2fa59c2c317890ff5c15178280de1db232
gnu3ra <alexandersballmer@gmail.com>
2016-09-26 00:41:30 -0500

    Improved test data

--- test.c
+++ test.c
@@ -6,7 +6,11 @@ int
 main(int argc, char *argv[])
 {
   sleep(1);
-  printf(1, "ret: %d\n", gticks());
-  printf(1, "tcount: %d\n", tcount());
+  printf(1, "Ticks from one second of sleep %d\n", gticks());
+  printf(1, "Traps so far (including this call) %d\n", tcount());
+  sleep(6);
+  printf(1, "Ticks from six second of sleep %d\n", gticks());
+  printf(1, "Traps so far (including this call) %d\n", tcount());
+  
   exit();
 }
