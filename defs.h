e37db7286bfc095277ea2b4ac59668e3a1d9309a
gnu3ra <alexandersballmer@gmail.com>
2016-09-25 22:46:44 -0500

    Added new syscall that does nothing

--- defs.h
+++ defs.h
@@ -182,5 +182,9 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
+//uncatagorized and lazy
+int gticks(void);
+
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
