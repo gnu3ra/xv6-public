26fde02d74711ca9de615e033f4bc41b6dbed82f
gnu3ra <alexandersballmer@gmail.com>
2016-10-06 23:32:56 -0500

    final work

--- test.c
+++ test.c
@@ -5,12 +5,24 @@
 int
 main(int argc, char *argv[])
 {
-  sleep(1);
-  printf(1, "Ticks from one second of sleep %d\n", gticks());
-  printf(1, "Traps so far (including this call) %d\n", tcount());
-  sleep(6);
-  printf(1, "Ticks from six second of sleep %d\n", gticks());
-  printf(1, "Traps so far (including this call) %d\n", tcount());
+  sleep(100);
+  printf(1, "[proccess1] Ticks from one second of sleep %d\n", gticks());
+  printf(1, "[proccess1] Traps so far (including this call) %d\n", tcount());
+  sleep(600);
+  printf(1, "[proccess1] Ticks from six second of sleep %d\n", gticks());
+  printf(1, "[proccess1] Traps so far (including this call) %d\n", tcount());
+
+  if(fork() == 0) {
+    sleep(100);
+    printf(1, "[process2] Ticks from one second of sleep %d\n", gticks());
+    printf(1, "[process2] Traps so far (including this call) %d\n", tcount());
+    sleep(600);
+    printf(1, "[process2] Ticks from six second of sleep %d\n", gticks());
+    printf(1, "[process2] Traps so far (including this call) %d\n", tcount());
+  }
+  else {
+    wait();
+  }
   
   exit();
 }
