49ccc8d7004fd4b7c9b0001973bc580999c072b3
gnu3ra <alexandersballmer@gmail.com>
2016-11-05 11:50:17 -0500

    Added new test data

--- test.c
+++ test.c
@@ -9,6 +9,7 @@ main(int argc, char *argv[])
   int * x;
   x =(unsigned long) 0x0;
   int y  = *x;
+  printf(1,"If you see \"no segfault\", then segfault has failed\n");
   printf(1,"pointer is %p\n value is %d \n",x, y);
 
   printf(1,"hello\n");
