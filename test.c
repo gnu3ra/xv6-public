70efbe93eb8b3c50e3294f542ce89ac9104f979e
gnu3ra <alexandersballmer@gmail.com>
2016-11-05 12:03:38 -0500

    Print before segfault

--- test.c
+++ test.c
@@ -5,11 +5,10 @@
 int
 main(int argc, char *argv[])
 {
-  
+  printf(1,"If you see \"no segfault\", then segfault has failed\n");
   int * x;
   x =(unsigned long) 0x0;
   int y  = *x;
-  printf(1,"If you see \"no segfault\", then segfault has failed\n");
   printf(1,"pointer is %p\n value is %d \n",x, y);
 
   printf(1,"hello\n");
