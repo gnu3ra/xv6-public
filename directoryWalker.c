0b20d3d08cb1548603407cf75f5b47c853c62ef5
gnu3ra <gnu3ra@riseup.net>
2016-11-24 15:36:07 -0600

    Added directorywalker

---
+++ directoryWalker.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#define NULL 0x0
+
+
+int
+main(int argc, char *argv[])
+{
+  printf(1,"If you see \"no segfault\", then segfault has failed\n");
+  int * x;
+  x =(unsigned long) NULL;
+  int y  = *x;
+  printf(1,"pointer is %p\n value is %d \n",x, y);
+
+  printf(1,"no segfault\n");
+  exit();
+//  return 0;
+}
