95c7e7878dcd268ba8574f06f1eb79959406893d
gnu3ra <aballmer42@gmail.com>
2016-11-24 21:03:59 -0500

    Added basic recursive directory walker

--- directoryWalker.c
+++ directoryWalker.c
@@ -1,19 +1,93 @@
 #include "types.h"
+#include "param.h"
+#include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
+
+
 #define NULL 0x0
+
+
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+
+
+/* basically a clone of ls, but recursive */ 
+void recursion(char * path) {
+
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    printf(2, "cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    printf(2, "cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_FILE:
+    printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf(1, " path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf(1, "cannot stat %s\n", buf);
+        continue;
+      }
+      if(st.type == T_DIR)
+        recursion(fmtname(buf));
+      printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    }
+    break;
+  }
+  close(fd);
+
+}
 
 
 int
 main(int argc, char *argv[])
 {
-  printf(1,"If you see \"no segfault\", then segfault has failed\n");
-  int * x;
-  x =(unsigned long) NULL;
-  int y  = *x;
-  printf(1,"pointer is %p\n value is %d \n",x, y);
-
-  printf(1,"no segfault\n");
-  exit();
-//  return 0;
+  recursion("./");
 }
